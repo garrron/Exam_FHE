ea78ae38712aeef172888eb0ad758cc07cf61eab
garrron <garrron@users.noreply.github.com>
2025-11-11 14:09:37 +0000

    Add resolver-database.rs, resolver.txt, spec.sol, mock-common.py

---
+++ frontend/web/src/backend-global-analytics/mock-common.py
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/exam_fhe/frontend/web/src/backend-global-analytics/mock-common.py
+// Generated at: 2025-11-27T12:25:20.920Z
+
+
